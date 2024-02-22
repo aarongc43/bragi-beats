9d8738215f4d90a08f49d8a2efea0b1b189750c6
aarongc43 <aarongc43@gmail.com>
2024-02-21 18:46:00 -0600

    27 lines of code to make a music player. How inventive.

--- main.c
+++ main.c
@@ -8,16 +8,20 @@ int main(void) {
 
     InitWindow(screenWidth, screenHeight, "Raylib Test Window");
 
-    SetTargetFPS(30);
+    SetTargetFPS(60);
+
+    InitAudioDevice();
+    Music music = LoadMusicStream("music/Mall Grab - Love Reigns.wav");
+    PlayMusicStream(music);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground((Color){ GetTime()*10.0f, GetTime()*15.0f, GetTime()*20.0f, 255 });
-            DrawText("Hello World!", 200, 200, 20, LIGHTGRAY);
+        ClearBackground((Color){ GetTime()*10.0f, GetTime()*15.0f, GetTime()*20.0f, 255 });
+        DrawText("Hello World!", 200, 200, 20, LIGHTGRAY);
+        UpdateMusicStream(music);
         EndDrawing();
 
     }
     CloseWindow();
-
     return 0;
 }
