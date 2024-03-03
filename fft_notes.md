d3f30cc7fa942189e79f75e00115e4b3d893842d
aarongc43 <aarongc43@gmail.com>
2024-03-03 12:40:08 -0600

    Created drag-and-drop feature

---
+++ fft_notes.md
@@ -0,0 +1,10 @@
+# Drag-and-Drop Music File Handling
+
+- Monitor Drag-and-Drop Events
+  `IsFileDropped()`
+  `GetDroppedFiles()`
+- Load and Play the Dropped Music Files
+  `LoadMusicStream()`
+  `PlayMusicStream()`
+
+
--- main.c
+++ main.c
@@ -1,27 +1,59 @@
 #include <stdio.h>
-
 #include <raylib.h>
 
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 800;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Test Window");
+    InitWindow(screenWidth, screenHeight, "Bragi Beats");
 
     SetTargetFPS(60);
 
     InitAudioDevice();
-    Music music = LoadMusicStream("music/Mall Grab - Love Reigns.wav");
-    PlayMusicStream(music);
+
+    Music music;
+    bool musicLoaded = false;
 
     while(!WindowShouldClose()) {
+        // Handle drag-and-drop
+        if (IsFileDropped()) {
+            FilePathList droppedFiles = LoadDroppedFiles();
+
+            if (droppedFiles.count > 0 && IsFileExtension(droppedFiles.paths[0], ".wav")) {
+                if (musicLoaded) {
+                    UnloadMusicStream(music);
+                }
+                music = LoadMusicStream(droppedFiles.paths[0]);
+                PlayMusicStream(music);
+                musicLoaded = true;
+            }
+
+            UnloadDroppedFiles(droppedFiles);
+        }
+
+        if (musicLoaded) {
+            UpdateMusicStream(music);
+        }
+
         BeginDrawing();
         ClearBackground((Color){ GetTime()*10.0f, GetTime()*15.0f, GetTime()*20.0f, 255 });
-        DrawText("Hello World!", 200, 200, 20, LIGHTGRAY);
-        UpdateMusicStream(music);
-        EndDrawing();
 
+        if (musicLoaded) {
+            DrawText("Playing music...", 10, 10, 20, LIGHTGRAY);
+        } else {
+            DrawText("Drag and Drop a Song to Start Playing", 200, 200, 20, LIGHTGRAY);
+        }
+
+        // Visualization Code
+
+        EndDrawing();
     }
+     if (musicLoaded) {
+        UnloadMusicStream(music);
+    }
+
+    CloseAudioDevice();
     CloseWindow();
+
     return 0;
 }
