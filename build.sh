2acf4ee5536dd228296fae013852de90d2566816
aarongc43 <aarongc43@gmail.com>
2024-03-04 19:03:36 -0600

    fft implementation and drawing rectangles based off on frequency amplitudes

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -xe
 
-CFLAGS="-Wall -Wextra -framework IOKit -framework Cocoa -framework OpenGL `pkg-config --libs --cflags raylib`"
+CFLAGS="-Wall -Wextra -framework IOKit -framework Cocoa -framework OpenGL `pkg-config --libs --cflags raylib` `pkg-config --libs --cflags fftw3`"
 
 cc $CFLAGS -o bragibeats main.c
-
-
